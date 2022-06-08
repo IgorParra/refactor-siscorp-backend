b335d407d4b2ae84c57c768b31236f82c2b5c596
MaiconBusinari <63322904+MaiconBusinari@users.noreply.github.com>
2022-06-08 19:02:59 -0300

    Update FindUserByIdOrEmailService

--- src/modules/user/services/FindUserByIdOrEmailService.ts
+++ src/modules/user/services/FindUserByIdOrEmailService.ts
@@ -6,8 +6,9 @@ import { User } from "../entities/User";
 export class FindUserByIdOrEmail {
 	async execute(param: string) {
 		const UsersRepository = AppDataSource.getRepository(User);
+		const regexUuid = /^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/gi;
 
-		const findById = isUid(param);
+		const findById = regexUuid.test(param);
 
 		const user = await UsersRepository.findOne({
 			where: findById ? { id: param } : { email: param },
