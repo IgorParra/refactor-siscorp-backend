2a101ba5cbd5d81621bc8fb7159204bd97aeb490
Igor Parra <igor.parra@rpinfo.com.br>
2022-06-19 12:21:15 -0300

    Melhorando service "UpdateProduct"

--- src/modules/product/routes/services/UpdateProductService.ts
+++ src/modules/product/routes/services/UpdateProductService.ts
@@ -1,23 +1,24 @@
 import { AppDataSource } from "../../../../shared/database";
 import { Product } from "../../../user/entities/Product";
 
-export class UpdateProductService {
-	async execute(barcode: string,
-        name: string,
-        brand: string,
-        description: string,
-        complementation: string) {
-		const ProductsRepository = AppDataSource.getRepository(Product);
+interface UpdateProductServiceProps {
+	barcode: string;
+	name: string;
+	brand: string;
+	description: string;
+	complementation: string;
+}
 
-		const product = await ProductsRepository.findOne({
+export class UpdateProductService {
+	async execute(newProductData: UpdateProductServiceProps) {
+		const ProductsRepository = AppDataSource.getRepository(Product);
+		const { barcode } = newProductData;
+
+		let product = await ProductsRepository.findOne({
 			where: { barcode },
 		});
 
-		product.name = name;
-		product.brand = brand;
-		product.description = description;
-        product.complementation =complementation;
-
+		product = { ...product, ...newProductData };
 		ProductsRepository.save(product);
 	}
 }
